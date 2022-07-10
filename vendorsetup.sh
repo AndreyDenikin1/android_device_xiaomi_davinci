fe05f7efaf800b1d12588ab144d3921de85bdfff
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-10 14:49:24 +0300

    davinci:change vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,4 +2,3 @@ git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git pr
 git clone -b 12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 git clone -b rice --depth=1 https://github.com/AndreyDenikin1/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
 git clone -b 12.1 --depth=1 https://github.com/AndreyDenikin1/vendor_xiaomi.git vendor/xiaomi
-git clone -b 12.1 --depth=1 https://github.com/AndreyDenikin1/vendor_xiaomi_miuicamera.git vendor/xiaomi
