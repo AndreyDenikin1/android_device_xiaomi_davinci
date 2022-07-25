af53ede5dd6378b20f5717deff2a59346f1f9705
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-25 18:05:53 +0300

    Rebase RiseDroid 3.5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 git clone -b 15 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-#git clone -b 12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
+git clone -b 12.1 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sm6150
 git clone -b rice --depth=1 https://github.com/AndreyDenikin1/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
 git clone -b rice --depth=1 https://github.com/AndreyDenikin1/android_device_xiaomi_davinci.git device/xiaomi/davinci
 git clone -b rice --depth=1 https://github.com/AndreyDenikin1/android_vendor_xiaomi_davinci.git vendor/xiaomi/davinci
